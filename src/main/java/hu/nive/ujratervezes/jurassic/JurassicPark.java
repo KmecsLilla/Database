79451728437c9cf52d5f259ce8ddd3928f4c4dec
KmecsLilla <kmecs.lilla@gmail.com>
2021-04-29 11:53:04 +0200

    Implement database task

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -12,4 +16,19 @@ public class JurassicPark {
         this.dbPassword = dbPassword;
     }
 
+    public List<String> checkOverpopulation() {
+        List<String> overpopulatedDinosaurs = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            String sql = "SELECT breed FROM dinosaur GROUP BY breed HAVING actual > expected ORDER BY breed ASC";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                overpopulatedDinosaurs.add(resultSet.getString(1));
+            }
+            return overpopulatedDinosaurs;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
